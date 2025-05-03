4bee5c56baf1c0bdeff16eb5a235e2e2b7ba53e6
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2025-05-03 12:45:27 +0200

    resetting state with key prop,batching,events

--- 08-how-react-works/src/App.js
+++ 08-how-react-works/src/App.js
@@ -39,7 +39,10 @@ function Tabbed({ content }) {
       </div>
 
       {activeTab <= 2 ? (
-        <TabContent item={content.at(activeTab)} />
+        <TabContent
+          key={content.at(activeTab).summary}
+          item={content.at(activeTab)}
+        />
       ) : (
         <DifferentContent />
       )}
@@ -63,7 +66,20 @@ function TabContent({ item }) {
   const [likes, setLikes] = useState(0);
 
   function handleInc() {
-    setLikes(likes + 1);
+    setLikes((likes) => likes + 1);
+  }
+
+  function handleTrippleInc() {
+    setLikes((likes) => likes + 3);
+  }
+
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0);
+  }
+
+  function handleUndoDelay() {
+    setTimeout(handleUndo, 2000);
   }
 
   return (
@@ -79,13 +95,13 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTrippleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoDelay}>Undo in 2s</button>
       </div>
     </div>
   );
