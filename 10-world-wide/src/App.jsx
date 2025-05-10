f7c142826adb47ce8c64ee1ee2b2d8d04e49e6e8
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2025-05-10 10:47:02 +0200

    building navigation and pages

--- 10-world-wide/src/App.jsx
+++ 10-world-wide/src/App.jsx
@@ -1,12 +1,25 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Product from './pages/Product';
+import Homepage from './pages/Homepage';
+import Pricing from './pages/Pricing';
+import PageNotFound from './pages/PageNotFound';
+import AppLayout from './pages/AppLayout';
+import Login from './pages/Login';
 
 function App() {
-
   return (
-     <div>
-
-     </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- 10-world-wide/vite.config.js
+++ 10-world-wide/vite.config.js
@@ -4,5 +4,16 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [
+    react(),
+    eslint({
+      // runs `eslint --fix` on your files
+      fix: true,
+      // only lint + fix these globs
+      include: ["src/**/*.js", "src/**/*.jsx", "src/**/*.ts", "src/**/*.tsx"],
+      // optional: show warnings/errors in browser overlay
+      emitWarning: true,
+      emitError: true,
+    }),
+  ],
 });
