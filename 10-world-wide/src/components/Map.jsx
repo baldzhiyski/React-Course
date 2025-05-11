c80c36ce45a22ffd44e2407d08be652425ec2311
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2025-05-11 20:03:43 +0200

    including a map

--- 10-world-wide/src/components/Map.jsx
+++ 10-world-wide/src/components/Map.jsx
@@ -1,7 +1,11 @@
 import styles from './Map.module.css';
 import { useNavigate, useSearchParams } from 'react-router-dom';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useState } from 'react';
+
 function Map() {
   const navigate = useNavigate();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
   const [searchParams, setSearchParams] = useSearchParams();
 
   const lat = searchParams.get('lat');
@@ -14,10 +18,22 @@ function Map() {
         navigate('form');
       }}
     >
-      <h1>Map</h1>
-      <h1>
-        Position : {lat} {lng}
-      </h1>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
