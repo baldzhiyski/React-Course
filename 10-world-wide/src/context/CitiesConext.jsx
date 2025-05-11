6d66044d6cccb20debf733c4a1701d2383c85f2b
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2025-05-11 19:50:16 +0200

    Finishing city detailed view

--- 10-world-wide/src/context/CitiesConext.jsx
+++ 10-world-wide/src/context/CitiesConext.jsx
@@ -10,6 +10,7 @@ const BASE_URL = 'http://localhost:8000';
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(function () {
     async function fetchCities() {
@@ -26,11 +27,27 @@ function CitiesProvider({ children }) {
 
     fetchCities();
   }, []);
+
+  async function getCityById(cityId) {
+    try {
+      const res = await fetch(`${BASE_URL}/cities/${cityId}`);
+
+      const data = await res.json();
+
+      console.log(data);
+      setCurrentCity(data);
+    } catch (e) {
+      alert('There was an error loading data ... ');
+    }
+  }
+
   return (
     <CitiesContext.Provider
       value={{
         cities: cities,
         isLoading: isLoading,
+        currentCity: currentCity,
+        getCityById: getCityById,
       }}
     >
       {children}
