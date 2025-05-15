ff7e355a71efa12982dea691a2332bef41e0be80
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2025-05-15 09:33:50 +0200

    adding fake auth login and logout functions

--- 10-world-wide/src/components/PageNav.jsx
+++ 10-world-wide/src/components/PageNav.jsx
@@ -1,7 +1,10 @@
 import { NavLink } from 'react-router-dom';
 import styles from './PageNav.module.css';
 import Logo from './Logo';
+import { useAuth } from '../context/FakeAuthContext';
+import Button from './Button';
 function PageNav() {
+  const { isAuthenticated, user } = useAuth();
   return (
     <nav className={styles.nav}>
       <Logo />
@@ -17,11 +20,17 @@ function PageNav() {
           <NavLink to="/product">Product</NavLink>
         </li>
 
-        <li>
-          <NavLink className={styles.ctaLink} to="/login">
-            Log In
-          </NavLink>
-        </li>
+        {isAuthenticated ? (
+          <li>
+            <Button type="primary">Log Out</Button>
+          </li>
+        ) : (
+          <li>
+            <NavLink className={styles.ctaLink} to="/login">
+              Log In
+            </NavLink>
+          </li>
+        )}
       </ul>
     </nav>
   );
--- 10-world-wide/src/pages/AppLayout.jsx
+++ 10-world-wide/src/pages/AppLayout.jsx
@@ -1,11 +1,15 @@
 import Map from '../components/Map';
 import SideBar from '../components/Sidebar';
+import User from '../components/User';
+import { useAuth } from '../context/FakeAuthContext';
 import styles from './AppLayout.module.css';
 function AppLayout() {
+  const { isAuthenticated } = useAuth();
   return (
     <div className={styles.app}>
       <SideBar />
       <Map />
+      {isAuthenticated ? <User /> : ''}
     </div>
   );
 }
