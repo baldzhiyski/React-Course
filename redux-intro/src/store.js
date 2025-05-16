6f07c0de7fe590cd515e609110d9f32444c70bc3
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2025-05-16 10:41:54 +0200

    converting the store with RTK

--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,13 +1,13 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
-import { thunk } from "redux-thunk";
 
-const rootReducer = combineReducers({
-  customer: customerReducer,
-  account: accountReducer,
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
