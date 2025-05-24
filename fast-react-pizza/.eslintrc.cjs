47be892e9b5ce574ba37d5af4ab2b2b9fd576aaf
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2025-05-24 17:09:25 +0200

    implementing routes (render-as-you-fetch)

--- fast-react-pizza/.eslintrc.cjs
+++ fast-react-pizza/.eslintrc.cjs
@@ -1,20 +1,27 @@
+// .eslintrc.cjs
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    // ↓ turn off “unused vars” checking entirely
+    "no-unused-vars": "off",
+
+    // or just warn instead of error:
+    // 'no-unused-vars': ['warn', { args: 'none', ignoreRestSiblings: true }],
+
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
