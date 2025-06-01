d4225961907f6166353db45b60271842eb855956
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2025-06-01 13:43:29 +0300

    some other css

--- fast-react-pizza/src/ui/Loader.jsx
+++ fast-react-pizza/src/ui/Loader.jsx
@@ -1,5 +1,12 @@
 function Loader() {
-  return <div className="loader"></div>;
+  return (
+
+  <div className="absolute bg-slate-200/40 inset-0  backdrop-blur-sm flex items-center justify-center">
+     <div className="loader"></div>
+  </div>
+  
+  )
+ 
 }
 
 export default Loader;
