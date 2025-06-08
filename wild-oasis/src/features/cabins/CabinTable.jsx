a5ffadfaf17e8bb37b68bce36c7373b1f0a3f69c
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2025-06-08 16:27:09 +0200

    fetching cabin data

--- wild-oasis/src/features/cabins/CabinTable.jsx
+++ wild-oasis/src/features/cabins/CabinTable.jsx
@@ -1,8 +1,11 @@
 import styled from "styled-components";
+import { useQuery } from "@tanstack/react-query";
+import { getCabins } from "../../services/apiCabins";
+import Spinner from "../../ui/Spinner";
+import CabinRow from "../cabins/CabinRow";
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
-
   font-size: 1.4rem;
   background-color: var(--color-grey-0);
   border-radius: 7px;
@@ -14,7 +17,6 @@ const TableHeader = styled.header`
   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
   column-gap: 2.4rem;
   align-items: center;
-
   background-color: var(--color-grey-50);
   border-bottom: 1px solid var(--color-grey-100);
   text-transform: uppercase;
@@ -23,3 +25,49 @@ const TableHeader = styled.header`
   color: var(--color-grey-600);
   padding: 1.6rem 2.4rem;
 `;
+
+function CabinTable() {
+  const {
+    data: cabins,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["cabin"],
+    queryFn: getCabins,
+  });
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return (
+      <div
+        role="alert"
+        style={{ padding: "1rem", color: "var(--color-error)" }}
+      >
+        Error loading cabins: {error.message}
+      </div>
+    );
+  }
+
+  return (
+    <Table role="table">
+      <TableHeader role="row">
+        <div></div>
+        <div>Cabin</div>
+        <div>Capacity</div>
+        <div>Price</div>
+        <div>Discount</div>
+        <div></div>
+      </TableHeader>
+
+      {cabins?.map((cabin) => (
+        <CabinRow cabin={cabin} key={cabin.id} />
+      ))}
+    </Table>
+  );
+}
+
+export default CabinTable;
--- wild-oasis/src/pages/Cabins.jsx
+++ wild-oasis/src/pages/Cabins.jsx
@@ -1,12 +1,19 @@
-import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import CabinTable from "../features/cabins/CabinTable";
+import Heading from "../ui/Heading";
 
 function Cabins() {
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All Cabins</Heading>
+        <p>Filter/Sort Here</p>
+      </Row>
+
+      <Row type="horizontal">
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
