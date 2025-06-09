d530e8717d5e140ff7d2289ba1749adc7830c497
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2025-06-09 15:59:44 +0200

    adding the form

--- wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -1,77 +1,42 @@
-import styled from "styled-components";
-
-import Input from "../../ui/Input";
-import Form from "../../ui/Form";
-import FormRow from "../../ui/FormRow";
-import Button from "../../ui/Button";
-import FileInput from "../../ui/FileInput";
-import Textarea from "../../ui/Textarea";
-import { useForm } from "react-hook-form";
-import { createCabin } from "../../services/apiCabins";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-hot-toast";
 
-// const FormRowSec = styled.div`
-//   display: grid;
-//   align-items: center;
-//   grid-template-columns: 24rem 1fr 1.2fr;
-//   gap: 2.4rem;
+import Input from "../../ui/Input";
+import Form from "../../ui/Form";
+import Button from "../../ui/Button";
+import FileInput from "../../ui/FileInput";
+import Textarea from "../../ui/Textarea";
+import FormRow from "../../ui/FormRow";
 
-//   padding: 1.2rem 0;
-
-//   &:first-child {
-//     padding-top: 0;
-//   }
-
-//   &:last-child {
-//     padding-bottom: 0;
-//   }
-
-//   &:not(:last-child) {
-//     border-bottom: 1px solid var(--color-grey-100);
-//   }
-
-//   &:has(button) {
-//     display: flex;
-//     justify-content: flex-end;
-//     gap: 1.2rem;
-//   }
-// `;
-
-const Label = styled.label`
-  font-weight: 500;
-`;
+import { useForm } from "react-hook-form";
+import { createCabin } from "../../services/apiCabins";
 
 function CreateCabinForm() {
-  const queryClient = useQueryClient();
   const { register, handleSubmit, reset, getValues, formState } = useForm();
-  const { errors } = formState();
+  const { errors } = formState;
 
-  const { mutate, isLoading } = useMutation({
+  const queryClient = useQueryClient();
+
+  const { mutate, isLoading: isCreating } = useMutation({
     mutationFn: createCabin,
     onSuccess: () => {
-      toast.success("New Cabin was successfully created !");
-      queryClient.invalidateQueries({
-        queryKey: ["caibns"],
-      });
+      toast.success("New cabin successfully created");
+      queryClient.invalidateQueries({ queryKey: ["cabins"] });
       reset();
     },
-    onError: (err) => {
-      toast.error(err.message);
-    },
+    onError: (err) => toast.error(err.message),
   });
 
-  function onSubmitForm(data) {
-    mutate(data);
+  function onSubmit(data) {
+    mutate({ ...data, image: data.image[0] });
   }
-
   return (
-    <Form onSubmit={handleSubmit(onSubmitForm)}>
+    <Form onSubmit={handleSubmit(onSubmit)}>
       <FormRow label="Cabin name" error={errors?.name?.message}>
         <Input
-          disabled={isLoading}
           type="text"
           id="name"
+          disabled={isCreating}
           {...register("name", {
             required: "This field is required",
           })}
@@ -80,26 +45,30 @@ function CreateCabinForm() {
 
       <FormRow label="Maximum capacity" error={errors?.maxCapacity?.message}>
         <Input
-          disabled={isLoading}
           type="number"
           id="maxCapacity"
+          disabled={isCreating}
           {...register("maxCapacity", {
             required: "This field is required",
             min: {
               value: 1,
-              message: "Capacity should be at least one",
+              message: "Capacity should be at least 1",
             },
           })}
         />
       </FormRow>
 
-      <FormRow label="regularPrice" error={errors?.regularPrice?.message}>
+      <FormRow label="Regular price" error={errors?.regularPrice?.message}>
         <Input
-          disabled={isLoading}
           type="number"
           id="regularPrice"
+          disabled={isCreating}
           {...register("regularPrice", {
             required: "This field is required",
+            min: {
+              value: 1,
+              message: "Capacity should be at least 1",
+            },
           })}
         />
       </FormRow>
@@ -107,36 +76,36 @@ function CreateCabinForm() {
       <FormRow label="Discount" error={errors?.discount?.message}>
         <Input
           type="number"
-          disabled={isLoading}
           id="discount"
+          disabled={isCreating}
           defaultValue={0}
           {...register("discount", {
             required: "This field is required",
             validate: (value) =>
               value <= getValues().regularPrice ||
-              "Discount should be less than regular price ! ",
+              "Discount should be less than regular price",
           })}
         />
       </FormRow>
 
       <FormRow
         label="Description for website"
+        disabled={isCreating}
         error={errors?.description?.message}
       >
         <Textarea
           type="number"
-          disabled={isLoading}
           id="description"
           defaultValue=""
+          disabled={isCreating}
           {...register("description", {
             required: "This field is required",
           })}
         />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor="image">Cabin photo</Label>
-        <FileInput id="image" accept="image/*" disabled={isLoading} />
+      <FormRow label="Cabin photo">
+        <FileInput id="image" accept="image/*" {...register("image")} />
       </FormRow>
 
       <FormRow>
@@ -144,7 +113,7 @@ function CreateCabinForm() {
         <Button variation="secondary" type="reset">
           Cancel
         </Button>
-        <Button>Edit cabin</Button>
+        <Button disabled={isCreating}>Add cabin</Button>
       </FormRow>
     </Form>
   );
--- wild-oasis/src/pages/Cabins.jsx
+++ wild-oasis/src/pages/Cabins.jsx
@@ -1,17 +1,27 @@
-import Row from "../ui/Row";
+import { useState } from "react";
 import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
+import Row from "../ui/Row";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <>
       <Row type="horizontal">
-        <Heading as="h1">All Cabins</Heading>
-        <p>Filter/Sort Here</p>
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
       </Row>
 
-      <Row type="horizontal">
+      <Row>
         <CabinTable />
+
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
