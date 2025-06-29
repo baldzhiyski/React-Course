929a4adb38d68770f22276cc967a50a09ad5b62e
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2025-06-29 19:58:01 +0200

    auth : user login with supabase

---
+++ wild-oasis/src/features/authentication/useLogin.js
@@ -0,0 +1,21 @@
+import { useMutation } from "@tanstack/react-query";
+import { login as loginAPI } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
+
+export function useLogin() {
+  const navigate = useNavigate();
+  const { mutate: login, isLoading } = useMutation({
+    mutationFn: ({ email, password }) => loginAPI({ email, password }),
+    onSuccess: (user) => {
+      console.log(user);
+      navigate("/dashboard");
+    },
+    onError: (err) => {
+      console.log(err);
+      toast.error("Invalid Credentials !");
+    },
+  });
+
+  return { login, isLoading };
+}
