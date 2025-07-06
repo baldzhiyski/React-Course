b60542e7dc447adb03e02b640948203d750b396b
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2025-07-06 17:22:35 +0200

    creating global layout and adding starter files

--- the-wild-oasis-website/src/app/page.js
+++ the-wild-oasis-website/src/app/page.js
@@ -3,7 +3,6 @@ import Navigation from "./components/Navigation";
 export default function Home() {
   return (
     <div>
-      <Navigation />
       <h1>The Wild Oasis. Welcome to paradise !</h1>
     </div>
   );
