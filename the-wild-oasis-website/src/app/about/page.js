a3b86113cd462d8c3b19108aa754b3c1e9c20c6e
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2025-07-09 19:32:10 +0200

    optimizing about page

--- the-wild-oasis-website/src/app/about/page.js
+++ the-wild-oasis-website/src/app/about/page.js
@@ -1,3 +1,8 @@
+import Image from "next/image";
+import Link from "next/link";
+import image1 from "../../../public/about-1.jpg";
+import image2 from "../../../public/about-2.jpg";
+
 export const metadata = {
   title: "About",
 };
@@ -33,14 +38,14 @@ export default function Page() {
       </div>
 
       <div className="col-span-2">
-        <img
-          src="/about-1.jpg"
+        <Image
+          src={image1}
           alt="Family sitting around a fire pit in front of cabin"
         />
       </div>
 
       <div className="col-span-2">
-        <img src="/about-2.jpg" alt="Family that manages The Wild Oasis" />
+        <Image src={image2} alt="Family that manages The Wild Oasis" />
       </div>
 
       <div className="col-span-3">
@@ -65,12 +70,12 @@ export default function Page() {
           </p>
 
           <div>
-            <a
+            <Link
               href="/cabins"
               className="inline-block mt-4 bg-accent-500 px-8 py-5 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
             >
               Explore our luxury cabins
-            </a>
+            </Link>
           </div>
         </div>
       </div>
