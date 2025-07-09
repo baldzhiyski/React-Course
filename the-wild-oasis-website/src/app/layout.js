6e269721010b25e8f3e77c70a0ac85c1f7ce2e1f
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2025-07-09 18:39:47 +0200

    some improvements

--- the-wild-oasis-website/src/app/layout.js
+++ the-wild-oasis-website/src/app/layout.js
@@ -2,6 +2,7 @@ import Logo from "./_components/Logo";
 import "@/app/_styles/globals.css";
 import Navigation from "./_components/Navigation";
 import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -20,14 +21,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
+        <Header />
 
-        {children}
+        <div className="flex-1 px-8 py-12 ">
+          <main className="max-w-7xl  mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
